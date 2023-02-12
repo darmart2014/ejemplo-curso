1088a4d83cb582cd536167b35bec89f95b2ae848
unknown <darmart2014@gmail.com>
2023-02-12 11:18:05 -0300

    primera subida

---
+++ Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace MiPrimerAplicacion
+{
+    partial class frmFormularioPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmFormularioPrincipal));
+            this.btnRun = new System.Windows.Forms.Button();
+            this.lblEtiqueta = new System.Windows.Forms.Label();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtNuevo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnRun
+            // 
+            this.btnRun.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnRun.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRun.Image = ((System.Drawing.Image)(resources.GetObject("btnRun.Image")));
+            this.btnRun.Location = new System.Drawing.Point(303, 349);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(156, 45);
+            this.btnRun.TabIndex = 0;
+            this.btnRun.Text = "RUN";
+            this.btnRun.UseVisualStyleBackColor = false;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // lblEtiqueta
+            // 
+            this.lblEtiqueta.AutoSize = true;
+            this.lblEtiqueta.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblEtiqueta.Font = new System.Drawing.Font("Vinegar Stroke_PersonalUseOnly", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEtiqueta.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblEtiqueta.Location = new System.Drawing.Point(151, 51);
+            this.lblEtiqueta.Name = "lblEtiqueta";
+            this.lblEtiqueta.Size = new System.Drawing.Size(112, 31);
+            this.lblEtiqueta.TabIndex = 1;
+            this.lblEtiqueta.Text = "Nombre";
+            this.lblEtiqueta.MouseLeave += new System.EventHandler(this.lblEtiqueta_MouseLeave);
+            this.lblEtiqueta.MouseMove += new System.Windows.Forms.MouseEventHandler(this.lblEtiqueta_MouseMove);
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtApellido.Location = new System.Drawing.Point(333, 62);
+            this.txtApellido.MaxLength = 6;
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(100, 20);
+            this.txtApellido.TabIndex = 2;
+            this.txtApellido.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtApellido_KeyPress);
+   //         this.txtApellido.MouseDown += new System.Windows.Forms.MouseEventHandler(this.txtApellido_MouseDown);
+            // 
+            // txtNuevo
+            // 
+            this.txtNuevo.Location = new System.Drawing.Point(333, 177);
+            this.txtNuevo.Multiline = true;
+            this.txtNuevo.Name = "txtNuevo";
+            this.txtNuevo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtNuevo.Size = new System.Drawing.Size(158, 84);
+            this.txtNuevo.TabIndex = 3;
+            this.txtNuevo.Leave += new System.EventHandler(this.txtNuevo_Leave);
+            // 
+            // frmFormularioPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(795, 475);
+            this.Controls.Add(this.txtNuevo);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.lblEtiqueta);
+            this.Controls.Add(this.btnRun);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmFormularioPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "My Primer Aplicacion";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmFormularioPrincipal_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.frmFormularioPrincipal_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Label lblEtiqueta;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtNuevo;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiPrimerAplicacion
+{
+    public partial class frmFormularioPrincipal : Form
+    {
+        public frmFormularioPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bienvenidos a C#");
+        }
+
+        private void frmFormularioPrincipal_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            MessageBox.Show("Chau Chau ...");
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            // MessageBox.Show("Se disparo el evento Click", "Atención");
+            // this.BackColor = Color.Blue;
+            if (txtApellido.Text == "")
+                txtApellido.BackColor = Color.Red;
+            else
+                txtApellido.BackColor = System.Drawing.SystemColors.Control;
+        }
+
+        private void frmFormularioPrincipal_Click(object sender, EventArgs e)
+        {
+            MouseEventArgs click = (MouseEventArgs)e;
+            if (click.Button == MouseButtons.Left)
+                MessageBox.Show("Presiono el botón Izquierdo", "Atención");
+            else if (click.Button == MouseButtons.Right)
+                MessageBox.Show("Presiono el Botón Derecho", "Atención");
+            else
+            if (click.Button == MouseButtons.Middle)
+                MessageBox.Show("Presiono el botón del Medio", "Atencion");
+        }
+
+        private void lblEtiqueta_MouseMove(object sender, MouseEventArgs e)
+        {
+            lblEtiqueta.BackColor = Color.Cyan;
+            lblEtiqueta.Cursor = Cursors.Hand;
+        }
+
+        private void lblEtiqueta_MouseLeave(object sender, EventArgs e)
+        {
+            lblEtiqueta.BackColor = System.Drawing.SystemColors.Control;
+            lblEtiqueta.Cursor = Cursors.Arrow;
+        }
+
+        private void txtApellido_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar < 48 || e.KeyChar > 57) && e.KeyChar != 8)
+                e.Handled = true;
+
+
+            
+        }
+
+        private void txtNuevo_Leave(object sender, EventArgs e)
+        {
+            MessageBox.Show("Tiene " + txtNuevo.Text.Length + " Caracteres");
+        }
+
+       
+    }
+}
